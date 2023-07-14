490892dbe041799c0530b016c3a44d7a9feffbfc
Rajesh604 <kanugurajesh3@gmail.com>
2023-07-15 03:48:29 +0530

    adding all the data

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 import os
+import cohere
+from email.message import EmailMessage
+import ssl
+import smtplib
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
-
+client = cohere.Client('S3tQc1i6m6N905AO5A85eNzhh8o0qLb4FLdIA9Fu')
+gmate_sender = "gmate4869@gmail.com"
+gmate_password = "flytcvfsbivpaeno"
 
 @app.route('/')
 def index():
@@ -29,6 +35,23 @@ def hello():
 
 @app.route('/send_email', methods=['POST'])
 def send_email():
-    return 'Email sent!'
+    prompt = "send a email to yashwanth"
+    generated_text = client.generate(prompt)
+    gmate_receiver = "kanugurajesh3@gmail.com"
+    subject = "Email from Gmate"
+    em = EmailMessage()
+    em['Subject'] = subject
+    em['From'] = gmate_sender
+    em['To'] = gmate_receiver
+    em.set_content(str(generated_text[0]))
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(gmate_sender, gmate_password)
+        server.send_message(em)
+        print("Email sent successfully")
+    return str(generated_text[0])
+
 if __name__ == '__main__':
    app.run()
